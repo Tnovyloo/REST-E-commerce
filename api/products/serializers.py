1264abafe04ea1aef30a0fd6317f8e092f3de229
TnovyLoo <loobrome@gmail.com>
2023-03-04 13:19:29 +0100

    Creating views

--- api/products/serializers.py
+++ api/products/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from products.model import Product, ProductCategory
+from products.models import Product, ProductCategory
 
 
 class ProductCategorySerializer(serializers.ModelSerializer):
--- api/products/views.py
+++ api/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, mixins
 
 from products.models import Product, ProductCategory
 from products.serializers import ProductSerializer, ProductCategorySerializer
@@ -11,4 +11,13 @@ class ProductCategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ProductCategory.objects.all()
     serializer_class = ProductCategorySerializer
     permission_classes = (permissions.AllowAny, )
+
+
+class ProductViewSet(viewsets.GenericViewSet,
+                     mixins.ListModelMixin):
+    """
+    List and Retrieve all products.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
