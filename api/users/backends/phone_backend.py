bddb8a0f7192e69243be57fc425d5cad9e142e3e
TnovyLoo <loobrome@gmail.com>
2023-03-10 11:37:21 +0100

    Creating User - TODO add SMTP GMAIL

--- api/users/backends/phone_backend.py
+++ api/users/backends/phone_backend.py
@@ -13,19 +13,21 @@ class PhoneNumberAuthBackend(ModelBackend):
     Custom authentication backend to login users using phone number.
     """
 
-    def authenticate(self, request, username=None, password=None):
-        try:
-            number = phonenumbers.parse(
-                username,
-                settings.PHONENUMBER_DEFAULT_REGION
-            )
-            if not phonenumbers.is_valid_number(number):
-                return
-            try:
-                user = User.objects.get(phone__phone_number=number)
-                if user.check_password(password):
-                    return user
-            except User.DoesNotExist:
-                return
-        except NumberParseException:
-            return
+    pass
+    # def authenticate(self, request, username=None, password=None):
+    #     try:
+    #         number = phonenumbers.parse(
+    #             username,
+    #             settings.PHONENUMBER_DEFAULT_REGION
+    #         )
+    #         if not phonenumbers.is_valid_number(number):
+    #             return
+    #         try:
+    #             user = User.objects.get(phone__phone_number=number)
+    #             if user.check_password(password):
+    #                 return user
+    #         except User.DoesNotExist:
+    #             return
+    #     except NumberParseException:
+    #         return
+
